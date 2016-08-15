b130e9fdeb1af425e9149e50964cd90a9f83eea3
Elijah <elijah.thomas@my.jcu.edu.au>
2016-08-15 14:41:42 +1000

    Adds name printing program

--- oddName.py
+++ oddName.py
@@ -1,4 +1,12 @@
 """
 Elijah
-
 """
+
+name = input("Enter your name")
+while len(name) < 1:
+    name = input("Invalid\nEnter your name")
+for i in range(0, len(name), 2):
+    print(name[i], "")
+print()
+
+
